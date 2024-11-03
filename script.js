cafece7715ea091bf585c7af6edab4b537d8ffee
antbrown999 <164958480+antbrown999@users.noreply.github.com>
2024-11-03 08:21:58 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function rollDice() {
+    let sides = document.getElementById('numSides').value
+    let result = getDiceResult(sides)document.getElementById('dice-result').innerText = 'you rolled a '+result
+}
+
+function getDiceResult(max) {
+    let result = math.ceil(Math.random() * max)
+    return result
+}
+
+for (let i =1; i< 100; i++) {
+    let testResult = getDiceResult(i)
+    if (testResult > i || testResult <1) throw Error("Invalid dice result "+testResult+" for max " +i)
+}
